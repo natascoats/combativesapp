503f7f3450dcef3fe2fd768e860882a28b0165d3
natascoats <natascoats@gmail.com>
2026-01-30 09:43:26 -0500

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import GrapplingTournamentApp from './App';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <GrapplingTournamentApp />
-  </React.StrictMode>
-);
