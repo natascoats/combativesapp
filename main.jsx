43028c1a6ba950e5760c760b879536824bf0c9d1
natascoats <natascoats@gmail.com>
2026-01-30 09:35:51 -0500

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import GrapplingTournamentApp from './App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GrapplingTournamentApp />
+  </React.StrictMode>
+);
