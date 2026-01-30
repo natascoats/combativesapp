4e5258f13286f33fb18bf03e8beef8e295fa7d02
natascoats <natascoats@gmail.com>
2026-01-30 10:13:58 -0500

    Delete src directory

--- src/main (1).jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import GrapplingTournamentApp from './App.jsx';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <GrapplingTournamentApp />
-  </React.StrictMode>
-);
